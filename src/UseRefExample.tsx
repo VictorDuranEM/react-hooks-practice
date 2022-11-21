8243064833ce3d8bdf98d530a84b9e6727856cae
Victor Duran <victor.duran.em@gmail.com>
2022-11-21 11:02:54 -0500

    Add useRef example

--- src/UseRefExample.tsx
+++ src/UseRefExample.tsx
@@ -3,16 +3,33 @@ import { useEffect, useRef, useState } from "react"
 export default function App() {
   const [name, setName] = useState('');
   const renderCount = useRef(1);
-  
+  const inputRef = useRef<HTMLInputElement>(null);
+  const prevName = useRef('');
+
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
+    console.log(renderCount.current)
+    console.log('Running useEffect for render count update')
   })
+
+  useEffect(() => {
+    prevName.current = name;
+    console.log('Running useEffect for prevName update');
+  }, [name])
+
+  function focus() {
+    if (inputRef.current !== null) {
+      inputRef.current.focus();
+    }
+  }
 
   return (
     <>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      <div>My name is {name}</div>
+      { console.log('Returning component contents') }
+      <input ref={inputRef} type="text" value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name} and it used to be {prevName.current}</div>
       <div>I rendered {renderCount.current} times</div>
+      <button onClick={focus}>Focus</button>
     </>
   )
 }
